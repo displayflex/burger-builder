af38e992bae81864be3d5a6e7d942ac632dba687
displayflex <3tonishere@gmail.com>
2019-10-16 22:42:09 +0300

    Added an auth form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -10,6 +10,7 @@ const NavigationItems = () => {
 			<NavigationItem link="/" exact>Burger Builder</NavigationItem>
 			<NavigationItem link="/checkout">Checkout</NavigationItem>
 			<NavigationItem link="/orders">Orders</NavigationItem>
+			<NavigationItem link="/auth">Authenticate</NavigationItem>
 		</ul>
 	);
 };
