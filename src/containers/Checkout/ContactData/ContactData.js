0f7b9d49c414bc1c5bd0daad9fff3ec4fce5efd6
displayflex <3tonishere@gmail.com>
2019-10-21 17:05:26 +0300

    Improved and optimized the project

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import Input from '../../../components/UI/Input/Input'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
+import { updateObject, checkValidity } from '../../../shared/utility'
 
 import classes from './ContactData.module.css'
 
@@ -114,42 +115,16 @@ class ContactData extends Component {
     this.props.onOrderBurger(order, this.props.token)
   }
 
-  checkValidity(value, rules) {
-    let isValid = true
-
-    if (!rules) {
-      return true
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
-    }
-
-    return isValid
-  }
-
   inputChangedHandler = (evt, inputIdentifier) => {
-    const updatedOrderForm = {
-      ...this.state.orderForm,
-    }
-    const updatedFormElement = {
-      ...updatedOrderForm[inputIdentifier],
-    }
-    updatedFormElement.value = evt.target.value
-    updatedFormElement.valid = this.checkValidity(
-      updatedFormElement.value,
-      updatedFormElement.validation
-    )
-    updatedFormElement.touched = true
-    updatedOrderForm[inputIdentifier] = updatedFormElement
+    const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+      value: evt.target.value,
+      valid: checkValidity(evt.target.value, this.state.orderForm[inputIdentifier].validation),
+      touched: true,
+    })
+
+    const updatedOrderForm = updateObject(this.state.orderForm, {
+      [inputIdentifier]: updatedFormElement,
+    })
 
     let formIsValid = true
     for (let inputIdentifier in updatedOrderForm) {
