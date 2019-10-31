a18ba4f50c299dfc7738fabe7595c609b1a07a52
followthewhiterabbit <3tonishere@gmail.com>
2019-10-31 16:57:29 +0300

    Changed modal transition using transition-group

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { CSSTransition } from 'react-transition-group'
 
-import classes from './Modal.module.css';
-import Aux from '../../../hoc/Auxiliary/Auxiliary';
-import Backdrop from '../Backdrop/Backdrop';
+import classes from './Modal.module.css'
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
+import Backdrop from '../Backdrop/Backdrop'
 
 class Modal extends Component {
-	shouldComponentUpdate(nextProps, nextState) {
-		return (
-			nextProps.show !== this.props.show ||
-			nextProps.children !== this.props.children
-		);
-	}
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show || nextProps.children !== this.props.children
+  }
 
-	render() {
-		return (
-			<Aux>
-				<Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-				<div
-					className={classes.Modal}
-					style={{
-						transform: this.props.show ? `translateY(0)` : `translateY(-100vh)`,
-						opacity: this.props.show ? `1` : `0`
-					}}
-				>
-					{this.props.children}
-				</div>
-			</Aux>
-		);
-	}
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <CSSTransition
+          in={this.props.show}
+          timeout={{
+            enter: 600,
+            exit: 400,
+          }}
+          mountOnEnter
+          unmountOnExit
+          classNames={{
+            enterActive: classes.ModalOpen,
+            exitActive: classes.ModalClosed,
+          }}
+        >
+          <div className={classes.Modal}>{this.props.children}</div>
+        </CSSTransition>
+      </Aux>
+    )
+  }
 }
 
-export default Modal;
+export default Modal
